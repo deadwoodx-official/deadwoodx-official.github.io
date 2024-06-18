dc4c8e46f564a29b3ff34a845774e92d5a83a342
Deadwoodxbruh <aakash.gudivada@gmail.com>
2024-06-18 23:08:47 +0530

    k

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
-const timerlabel = document.getElementById("timer");
+const apiKey = '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'; // Replace 'YOUR_API_KEY' with your actual API key
 
-function formatNumberWithTwoDigits(number){
-    return String(number).padStart(2, '0');
+const data = {
+  sender: {
+    name: 'WoodxPortfolio.com',
+    email: 'botcom172@gmail.com'
+  },
+  to: [
+    {
+      email: 'aakash.gudivada@gmail.com'
+    }
+  ],
+  subject: 'noreply',
+  htmlContent: `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>WoodxPortfolio.com</title>
+  </head>
+  <body style="font-family: Arial, sans-serif;">
+    <div style="text-align: left; padding: 20px;">
+      <h1 style="margin: 0; font-size: 24px;">WoodxPortfolio.com</h1>
+    </div>
+    <div style="padding: 20px;">
+      <p>Your enquiry has been successfully sent to Deadwoodx. Thank you for reaching out!</p>
+      <p>Most of the are answered within 48 hours. Our team is dedicated to providing a thorough and thoughtful response tailored to your needs.</p>
+      <p>If you have any urgent concerns or additional information to share, feel free to raise another ticket at our website!.</p>
+      <p>Best Regards,<br>WoodxPortfolio.com Team</p>
+    </div>
+  </body>
+  </html>
+`
+};
+
+let cansendmail = true;
+
+function sendmail(){
+  if (cansendmail === true){
+    fetch('https://api.brevo.com/v3/smtp/email', {
+      method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    ' api-key': apiKey
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+  }
 }
 
-function updateTimer() {
-    const targetEpochTime = 1705147200;
-    const currentTime = Math.floor(new Date().getTime() / 1000);
-    const remainingSeconds = Math.max(targetEpochTime - currentTime, 0);
+function getElementsByClass(classnamegiven) {
+  var elements = document.querySelectorAll(classnamegiven);
+  var elementslist = {};
 
-    const days = Math.floor(remainingSeconds / 86400);
-    const hours = Math.floor((remainingSeconds % 86400) / 3600);
-    const minutes = Math.floor(((remainingSeconds%86400)%3600)/60);
-    const seconds = remainingSeconds % 60;
-    timerlabel.textContent = formatNumberWithTwoDigits(days) + ":" + formatNumberWithTwoDigits(hours) + ":" + formatNumberWithTwoDigits(minutes) + ":" + formatNumberWithTwoDigits(seconds);
+  for (var i = 0; i < elements.length; i++) {
+    elementsArray.push(elements[i]);
+  }
+
+  elementsArray.forEach(function(element) {
+    element.classList.toggle('offscreen');
+  });
 }
 
-updateTimer();
-setInterval(updateTimer, 1000);
+const contactbutton = document.getElementById("ContactMeButton");
+const aboutmebutton = document.getElementById("AboutMeButton");
+const freeservicesbutton = document.getElementById("ServicesButton");
+const previousworkbutton = document.getElementById("PreviousWorkButton")
+
+aboutmebutton.addEventListener('click',function(event){
+  getElementsByClass("ContactMePage")
+})
