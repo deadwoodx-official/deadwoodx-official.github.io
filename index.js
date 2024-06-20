db7257395635ac118785df2f41fc2938af9f634a
Deadwoodxbruh <aakash.gudivada@gmail.com>
2024-06-20 23:55:19 +0530

    k

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const button = document.getElementById("menu");
 const menu = document.getElementById("topbar");
 
+const skillsButton = document.getElementById("SkillsButton");
+let device = null;
+
 button.addEventListener("click",function(){
     if (menu.style.opacity == 1){
         menu.style.opacity = 0
     }else{
         menu.style.opacity = 1
     }
+})
+
+skillsButton.addEventListener("click",async function(){
+    window.location.href = "/activity/index.html"; 
 })
