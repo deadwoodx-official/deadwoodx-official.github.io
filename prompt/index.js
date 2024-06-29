44d5be7edf166f1452ea8fe3e971a8b969801595
Deadwoodxbruh <aakash.gudivada@gmail.com>
2024-06-29 18:37:32 +0530

    k

--- prompt/index.js
+++ prompt/index.js
@@ -11,7 +11,7 @@ const button3 = document.getElementById("3");
 const buttons = [button1,button2,button3]
 const code = fetch(`https://pricey-butternut-pear.glitch.me/ccode?dc=${devicecode}`);
 console.log(code);
-
+console.log(chosenbutton);
 for (let i = 0;i < 3;i++){
     let buttonrn = buttons[i];
     if (i === chosenbutton){
