8c7319ab81377243318aff40ed35f185c215f66e
Deadwoodxbruh <aakash.gudivada@gmail.com>
2024-06-29 20:49:33 +0530

    ok

--- prompt/index.js
+++ prompt/index.js
@@ -10,14 +10,20 @@ const button1 = document.getElementById("1");
 const button2 = document.getElementById("2");
 const button3 = document.getElementById("3");
 
-const buttons = [button1,button2,button3]
-const code = fetch(`https://pricey-butternut-pear.glitch.me/ccode?dc=${devicecode}`);
-console.log(code);
-console.log(chosenbutton);
+const buttons = [button1,button2,button3];
+
+fetch(`https://pricey-butternut-pear.glitch.me/ccode?dc=${devicecode}`)
+.then(response =>{
+    if (!response.ok){
+        console.log("error")
+    }else{
+        console.log(response.json())
+    }
+})
 for (let i = 0;i < 3;i++){
     let buttonrn = buttons[i];
     if (i === chosenbutton){
-        buttonrn.textContent = code;
+        buttonrn.textContent = "meow";
     }else{
         buttonrn.textContent = Math.round(Math.random()*200)
     }
