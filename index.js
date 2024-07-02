ae02ad95f9fd55179be73a42ef7448a11ce67454
Deadwoodxbruh <aakash.gudivada@gmail.com>
2024-07-02 22:46:06 +0530

    ok

--- index.js
+++ index.js
@@ -3,14 +3,39 @@ const laptopButton = document.getElementById('laptop');
 const qrcodeframe = document.getElementById("qrcode");
 const codetext = document.getElementById("code");
 const laptopui = document.querySelector('.laptopui');
-const connectedlabel = document.getElementById('connectedstatus')
+const connectedlabel = document.getElementById('connectedstatus');;
+const loadingtext = document.getElementById("connectionstatus");
 let connected = false;
+let loading = false;
+let count = 0;
+let dot = ".";
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function startLoading(){
+    count = 0;
+    loadingtext.style.display = "flex"
+    while (loading){
+        if (count > 2){
+            count = 0
+        }else{
+            count++
+        }
+        loadingtext.textContent = "CONNECTING" + dot.repeat(count);
+        await sleep(500)
+    }
+}
 
 laptopButton.addEventListener("click",function(){
     if (!websocket || websocket.readyState !== WebSocket.OPEN){
+        loading = true;
+        startLoading();
         websocket = new WebSocket("wss://pricey-butternut-pear.glitch.me");
         websocket.addEventListener("open",async function(event){
-            console.log("websocket connected");
+            loading = false;
+            loadingtext.textContent = "Created QR Code for connecting mobile."
             connected = true;
             websocket.send(JSON.stringify({message: 'laptopconnect'}))
         });
